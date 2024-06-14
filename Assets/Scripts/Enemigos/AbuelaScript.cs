3532644f9a5ad212a66c0753373964ce6c2826a5
Pollogamerz <tbarajasglez05@gmail.com>
2024-06-13 19:19:52 -0600

    cambios

--- Assets/Scripts/Enemigos/AbuelaScript.cs
+++ Assets/Scripts/Enemigos/AbuelaScript.cs
@@ -4,26 +4,57 @@ using UnityEngine;
 
 public class AbuelaScript : MonoBehaviour
 {
-    public GameObject GatoBola;
     public GameObject GaboBolacopia;
     public int rutine;
     public float chronometer;
     public Quaternion angle;
     public float grade;
+    private List<GameObject> players;
+    private GameObject closestPlayer;
 
     void Start()
     {
-        GatoBola = GameObject.FindGameObjectWithTag("Player");
+        UpdatePlayerList();
     }
 
     void Update()
     {
+        UpdatePlayerList();
         Enemie_Behavior();
+    }
+
+    void UpdatePlayerList()
+    {
+        // Find all players with the tag "Player" and store them in the list
+        players = new List<GameObject>(GameObject.FindGameObjectsWithTag("Player"));
+    }
+
+    GameObject GetClosestPlayer()
+    {
+        GameObject closest = null;
+        float minDistance = Mathf.Infinity;
+        Vector3 currentPosition = transform.position;
+
+        foreach (GameObject player in players)
+        {
+            float distance = Vector3.Distance(player.transform.position, currentPosition);
+            if (distance < minDistance)
+            {
+                closest = player;
+                minDistance = distance;
+            }
+        }
+
+        return closest;
     }
 
     public void Enemie_Behavior()
     {
-        if (Vector3.Distance(transform.position, GatoBola.transform.position) > 2)
+        closestPlayer = GetClosestPlayer();
+
+        if (closestPlayer == null) return;
+
+        if (Vector3.Distance(transform.position, closestPlayer.transform.position) > 2)
         {
             chronometer += 1 * Time.deltaTime;
             if (chronometer >= 4)
@@ -46,34 +77,9 @@ public class AbuelaScript : MonoBehaviour
                     break;
             }
         }
-
-        if (Vector3.Distance(transform.position, GaboBolacopia.transform.position) > 2)
-        {
-            chronometer += 1 * Time.deltaTime;
-            if (chronometer >= 4)
-            {
-                rutine = Random.Range(0, 2);
-                chronometer = 0;
-            }
-
-            switch (rutine)
-            {
-                case 0:
-                    grade = Random.Range(0, 360);
-                    angle = Quaternion.Euler(0, grade, 0);
-                    rutine++;
-                    break;
-
-                case 1:
-                    transform.rotation = Quaternion.RotateTowards(transform.rotation, angle, 0.5f);
-                    transform.Translate(Vector3.forward * 1 * Time.deltaTime);
-                    break;
-            }
-        }
-
         else
         {
-            var lookPos = GatoBola.transform.position - transform.position;
+            var lookPos = closestPlayer.transform.position - transform.position;
             lookPos.y = 0;
             var rotation = Quaternion.LookRotation(lookPos);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 3);
--- Assets/Scripts/GatoGordo/VidaGato.cs
+++ Assets/Scripts/GatoGordo/VidaGato.cs
@@ -2,12 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class VidaGato : MonoBehaviour
 {
     public float vida = 100;
     public Image barraDevida;
+    public GameObject gameOverPanel;
+    public Button respawnButton;
+
+    private Vector3 initialPosition;
+
+    void Start()
+    {
+        // Guarda la posición inicial del personaje
+        initialPosition = transform.position;
+
+        // Asegúrate de que el panel de "Game Over" esté desactivado
+        gameOverPanel.SetActive(false);
+
+        // Añade un listener al botón de "Reaparecer"
+        respawnButton.onClick.AddListener(Respawn);
+    }
 
     void Update()
     {
@@ -17,9 +32,25 @@ public class VidaGato : MonoBehaviour
 
         if (vida <= 0)
         {
-            SceneManager.LoadScene("Gameover");
+            ShowGameOverPanel();
         }
-
     }
 
+    void ShowGameOverPanel()
+    {
+        // Muestra el panel de "Game Over"
+        gameOverPanel.SetActive(true);
+    }
+
+    void Respawn()
+    {
+        // Restaura la vida
+        vida = 100;
+
+        // Oculta el panel de "Game Over"
+        gameOverPanel.SetActive(false);
+
+        // Restaura la posición inicial del personaje
+        transform.position = initialPosition;
+    }
 }
